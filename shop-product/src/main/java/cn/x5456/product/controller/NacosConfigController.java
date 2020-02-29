91d9911b4b6a876e15739f7bbbccda080641fc49
yujx <yujx@dist.com.cn>
2020-02-29 13:39:00 +0800

    配置中心

---
+++ shop-product/src/main/java/cn/x5456/product/controller/NacosConfigController.java
@@ -0,0 +1,31 @@
+package cn.x5456.product.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RefreshScope//动态刷新的注解
+public class NacosConfigController {
+
+    @Autowired
+    private ConfigurableApplicationContext applicationContext;
+
+    @Value("${config.appName}")
+    private String appName;
+
+
+    @RequestMapping("/test-config1")
+    public String testConfig1() {
+        return applicationContext.getEnvironment().getProperty("config.appName");
+    }
+
+
+    @RequestMapping("/test-config2")
+    public String testConfig2() {
+        return appName;
+    }
+}
